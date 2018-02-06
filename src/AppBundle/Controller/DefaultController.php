e27945a41516c4e3cab9b306bdbb1e11a3f8127d
ThePoloQ <polosky.tdb@gmail.com>
2018-02-06 18:17:41 +0100

    graph

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,7 +23,7 @@ class DefaultController extends Controller
     public function dashboardAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $q = $em->getRepository('AppBundle:Joueur')
+        $q1 = $em->getRepository('AppBundle:Joueur')
         ->createQueryBuilder('j')
         ->select("COUNT(1) as nb")
         ->addSelect("j.dateInscription as date")
@@ -31,29 +31,49 @@ class DefaultController extends Controller
         ->orderBy('j.dateInscription','ASC')
         ->getQuery();
 
-        $res = $q->getResult();
-        /*
-        [
-    			{ x: 10, y: 10 },
-    			{ x: 20, y: 14 },
-    			{ x: 30, y: 18 },
-    			{ x: 40, y: 22 },
-    			{ x: 50, y: 18 },
-    			{ x: 60, y: 28 }
-    		]*/
+        $res1 = $q1->getResult();
+
+        $q2 = $em->getRepository('AppBundle:Joueur')
+        ->createQueryBuilder('j')
+        ->select("COUNT(1) as nb")
+        ->addSelect("j.club as club")
+        ->groupBy("j.club")
+        ->orderBy('nb','ASC')
+        ->getQuery();
+
+        $res2 = $q2->getResult();
+
+        $q3 = $em->getRepository('AppBundle:Joueur')
+        ->createQueryBuilder('j')
+        ->select("COUNT(1) as nb")
+        ->addSelect("j.sexe as sexe")
+        ->groupBy("j.sexe")
+        ->orderBy('nb','DESC')
+        ->getQuery();
+
+        $res3 = $q3->getResult();
+
+
         $data1 = array();
-        foreach ($res as $row) {
+        foreach ($res1 as $row) {
           $date = $row['date'];
           $data1[] = array( "label" => $date->format('d-M'), "y" => \intval($row['nb']));
         }
 
-        echo '<pre>';
-        //var_dump(json_encode($data1));
-        echo '</pre>';
-        //die();
+        $data2 = array();
+        foreach ($res2 as $row) {
+          $data2[] = array( "label" => $row['club'], "y" => \intval($row['nb']));
+        }
+
+        $data3 = array();
+        foreach ($res3 as $row) {
+          $data3[] = array( "indexLabel" => $row['sexe'], "y" => \intval($row['nb']));
+        }
 
         return $this->render('dashboard.html.twig', array(
             'data1' => $data1,
+            'data2' => $data2,
+            'data3' => $data3,
         ));
     }
 }
